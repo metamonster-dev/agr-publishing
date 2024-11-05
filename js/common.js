d6df813ea36b070eadc5b44e5d61f7e986d86ffd
hdy86 <dyhong86@naver.com>
2024-11-05 21:43:05 +0900

    feat: 통합검색, 자동완성, 결과 페이지 퍼블리싱

--- js/common.js
+++ js/common.js
@@ -10,8 +10,15 @@ $(window).on("load", function () {
     slidesPerView: "auto",
     spaceBetween: 8,
   });
+  const tabSlider02 = new Swiper(".tab_slider02", {
+    slidesPerView: "auto",
+    spaceBetween: 0,
+  });
   $(".tab_area button").on("click", function () {
     $(this).parent().addClass("on").siblings().removeClass("on");
-    tabSlider.slideTo($(this).parent().index());
+    if ($(".tab_slider").length > 0)
+      tabSlider.slideTo($(this).parent().index());
+    if ($(".tab_slider02").length > 0)
+      tabSlider02.slideTo($(this).parent().index());
   });
 });
